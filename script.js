4c4b30d884d438b3efb9c7952a25485032f01b2a
Drelka <47503020+Drelka@users.noreply.github.com>
2024-03-17 20:01:31 +0000

    Add functions necessary for calc to work
    Added functions and event listeners:
    -calcultaing functions:
        -percentage,
        -root,
        -add,
        -subtract,
        -multiply,
        -divide,
    -handling error function:
        -receiving error on the screen when there's more than 15 digits
        on the screen,
    -making the dot "." work
    -operate() function, choosing the type of the operation based on the
    operator inner text
    -equals button event listener, so all the buttons work now

--- script.js
+++ script.js
@@ -8,27 +8,102 @@ const divide = document.querySelector(".divide");
 const subtract = document.querySelector(".subtract");
 const equals = document.querySelector(".equals");
 
-const screen = document.querySelector("#curr-input");
 const buttons = document.querySelectorAll(".buttons");
 const inputButtons = document.querySelectorAll(".input-buttons");
 const numerical = document.querySelectorAll(".numerical");
 const operators = document.querySelectorAll(".operators");
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        button.style.borderColor = "";
-    })
-});
-
-numerical.forEach(number => {
-    number.addEventListener("click", () => {
-        screen.innerText += number.innerText;
-        if (screen.innerText.length > 15) return screen.innerText = "Error";
-    });
-});
+const currInput = document.querySelector("#curr-input");
+const currOperation = document.querySelector("#curr-operation")
 
 let firstOperand = "";
 let secondOperand = "";
-let operator = "";
+let currOperator = null;
+let score = "";
 
-// add = function(first)
+errorReset = function() {
+    if (currInput.innerText === "Error") currInput.innerText = "";
+}
+
+inputTooLongError = function (x) {
+    if (x.innerText.length > 15) currInput.innerText = "Error";    
+}
+
+numerical.forEach(number => {
+    number.addEventListener("click", () => {
+        errorReset();
+        currInput.innerText += number.innerText;
+        inputTooLongError(currInput);
+    });    
+});
+
+dot.addEventListener("click", () => {
+    if (!currInput.innerText.includes(".")) {
+        return currInput.innerText += dot.innerText; 
+    }   
+});
+
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        currOperator = operator.innerText;
+        let botScreenValue = currInput;
+        if (currInput.innerText !== "") {
+            console.log(currInput.innerText.toString());
+            currOperation.innerText = botScreenValue.innerText + " " + currOperator;
+        } else {
+            x = currOperation.innerText; 
+            currOperation.innerText = x.slice(0, -1) + currOperator; 
+        }
+        if (currOperation.innerText !== "") currInput.innerText = "";
+    });
+});
+
+const percentageOperation = function(firstOperand, secondOperand) {
+    return (firstOperand / 100) * secondOperand;
+}    
+
+const rootOperation = function(firstOperand, secondOperand) {
+    return firstOperand ** (1 / secondOperand);
+}    
+
+const addOperation = function(firstOperand, secondOperand) {
+    return firstOperand + secondOperand;
+}    
+
+const subtractOperation = function(firstOperand, secondOperand) {
+    return firstOperand - secondOperand;
+}    
+
+const multiplyOperation = function(firstOperand, secondOperand) {
+    return firstOperand * secondOperand;
+}    
+
+const divideOperation = function(firstOperand, secondOperand) {
+    return secondOperand === 0? alert("Can't divide by zero!") : firstOperand / secondOperand;
+}    
+
+
+const operate = function(currOperator) {
+    if (currOperator === "%") return percentageOperation(firstOperand, secondOperand);
+    if (currOperator === "√") return rootOperation(firstOperand, secondOperand);
+    if (currOperator === "+") return addOperation(firstOperand, secondOperand);
+    if (currOperator === "-") return subtractOperation(firstOperand, secondOperand);
+    if (currOperator === "×") return multiplyOperation(firstOperand, secondOperand);
+    if (currOperator === "÷") return divideOperation(firstOperand, secondOperand);
+}
+
+singleOperation = equals.addEventListener("click", () => {
+    
+    if (!currOperation.innerText.includes("=")) {
+        let botScreenValue = currInput.innerText;
+        
+        firstOperand = +(currOperation.textContent.slice(0, -2));
+        secondOperand = +(botScreenValue);
+        currOperator = currOperation.textContent.slice(this.length - 1).toString();
+        
+        score = operate(currOperator);
+        currOperation.innerText += " " + botScreenValue + " =";
+    }
+    currInput.innerText = Math.round(score *10000) / 10000;
+    inputTooLongError(currInput);
+});
